1d86863b57e043eccbfd18a4e8057d44c0f67a74
FG-Corporation <71612422+FG-Corporation@users.noreply.github.com>
2020-12-07 02:17:16 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,38 @@
+// ! закрытие меню при нажатии
+$('.header__list').click(function(event) {
+    $('.header__burger,.header__menu').removeClass('active');
+    $('body').removeClass('lock');});
+// ! мигание
+$(document).ready(function() {
+    $('.header__burger').click(function(event) {
+        $('.header__burger,.header__menu').toggleClass('active');
+        $('body').toggleClass('lock');
+    });
+});
+// ! плавный скролл
+$(document).ready(function(){
+    $("#menu").on("click","a", function (event) {
+        event.preventDefault();
+        var id  = $(this).attr('href'),
+            top = $(id).offset().top;
+        $('body,html').animate({scrollTop: top}, 1000);
+    });
+});
+
+$(document).ready(function(){
+    $("#menu2").on("click","a", function (event) {
+        event.preventDefault();
+        var id  = $(this).attr('href'),
+            top = $(id).offset().top;
+        $('body,html').animate({scrollTop: top}, 1000);
+    });
+});
+
+$(document).ready(function(){
+    $("#menu3").on("click","a", function (event) {
+        event.preventDefault();
+        var id  = $(this).attr('href'),
+            top = $(id).offset().top;
+        $('body,html').animate({scrollTop: top}, 1000);
+    });
+});
